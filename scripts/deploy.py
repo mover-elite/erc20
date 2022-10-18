40f7d1330f1f90805289db857048423ef744f1ef
Ttech5 <ttech7633@gmail.com>
2022-10-19 00:56:38 +0100

    Create a simple erc20 token

---
+++ scripts/deploy.py
@@ -0,0 +1,9 @@
+from brownie import accounts, ERC20
+
+
+
+def main():
+    acct = accounts[0]
+    decimals = 18
+    token = ERC20.deploy("Test", "TST", decimals, 1000 * 10 ** decimals, {"from": acct})
+    print(token.balanceOf(acct))
